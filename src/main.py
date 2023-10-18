625a1bd05d664f0537372f2637be55e70d6f9d08
Local Administrator <LocalAdmin@TUD263544.tudelft.net>
2023-10-18 15:42:53 +0200

    Add multiplication function in main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 '''
 Combining operations
 '''
-from operations import summation, subtraction
+from operations import summation, subtraction, multiplication, division
 
 def perform_operation(num1, num2, operation):
     if operation == "add":
@@ -12,3 +12,12 @@ def perform_operation(num1, num2, operation):
         raise ValueError("Invalid operation. Please choose 'add' or 'subtract'.")
 
     return result
+
+def perform_mult_div(num1,num2,operation):
+	if operation =="multiply":
+		result == multiplication(num1,num2)
+	elif operation == "divide":
+		result = division(num1,num2)
+	else:
+		raise ValueError("Invalid operation. Please choose 'multiply' or 'divide'")
+	return result
